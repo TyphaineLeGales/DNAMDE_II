9df0f7cb620610a73cbcb548ce0b14d467adc819
paulinovart <pauline.dierick@gmail.com>
2026-06-22 16:03:52 +0200

    style fix

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,22 +1,22 @@
-import "./card.css"
+import "./card.css";
 
 function Card(props) {
-    console.log(props)
+  console.log(props);
 
-
-    return (
-      <div className="card">
-        <img src= {props.data.Poster} alt="" className = "movie-picture"/>
-        <h1 className = "movie-title"> {props.data.Title} </h1>
-        <div className="movie-date-rate-container">
-            <p className = "movie-date">{props.data.Year}</p>
-            <div  className="movie-rate"><p>⭐ {props.data.imdbRating}</p></div>
+  return (
+    <div className="card">
+      <img src={props.data.Images[0]} alt="" className="movie-picture" />
+      <h1 className="movie-title"> {props.data.Title} </h1>
+      <div className="movie-date-rate-container">
+        <p className="movie-date">{props.data.Year}</p>
+        <div className="movie-rate">
+          <p>⭐ {props.data.imdbRating}</p>
         </div>
-        <p className = "movie-genres">{props.data.Genre}</p>
-        <p className = "movie-synopsis"> {props.data.Plot}</p>
       </div>
-    )
-  }
-  
-  export default Card
-  
+      <p className="movie-genres">{props.data.Genre}</p>
+      <p className="movie-synopsis"> {props.data.Plot}</p>
+    </div>
+  );
+}
+
+export default Card;
