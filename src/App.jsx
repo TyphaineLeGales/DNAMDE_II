5dfd29b56874b2971bfad4adb161238e5eca7031
saraaaaaDM <desruelsarapro@gmail.com>
2026-06-22 15:33:07 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import FilmList from "./components/FilmList";
-
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Shadows+Into+Light+Two&display=swap');
+</style>
 function App() {
   return (
     <div>
