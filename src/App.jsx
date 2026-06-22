88d03e9da33e26f22ce6184751771e991a20cd70
saraaaaaDM <desruelsarapro@gmail.com>
2026-06-22 14:14:36 +0200

    Add film data and update app structure
    - Introduced a new filmData.json file containing a list of films with their titles, release dates, and images.
    - Updated App.jsx to include a FilmList component and changed the greeting to "Bonjour Sara".
    - Removed the old filmData.json file from assets to streamline data management.
    - Modified index.css to implement a grid layout for film cards and added styling for film images.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import FilmList from "./components/FilmList";
+
 function App() {
   return (
     <div>
-      <h1> Hello 🌞 </h1>
+      <h1>Bonjour Sara</h1>
+
+      <FilmList />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
