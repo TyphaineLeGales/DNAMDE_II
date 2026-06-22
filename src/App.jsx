1f406c45385f3f5b75ff571f7df992bf06ae2ffc
lucileguillo <lucile.guillo27@gmail.com>
2026-06-22 16:28:56 +0200

    added movies card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import filmData from './assets/filmData.json?raw';
+import FilmList from './components/filmList'
+
+
+const jsonData = JSON.parse(filmData);
+console.log(filmData)
+
 function App() {
+
   return (
-    <div>
-      <h1> Hello 🌞 </h1>
+    <div className='films-container'>
+      <FilmList film={jsonData}/>
     </div>
   )
+
 }
 
 export default App
