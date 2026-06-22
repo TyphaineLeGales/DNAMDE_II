0b06ebaff891dad1b3cc98e4c95959e5b94ca9bb
timolindstrom95-ops <timolindstrom95@gmail.com>
2026-06-22 17:33:52 +0200

    feat: align item

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import CardList from './components/CardList';
+import filmData from './assets/filmData.json?raw';
+import './App.css'
+import { useState } from 'react';
+
+let jsonData = JSON.parse(filmData);
+console.log(jsonData)
+
+
+
 function App() {
+  const [filtered, setFiltered] = useState(jsonData);
+
+  function handleChange(e) {
+    console.log(e, e.target.value)
+    setFiltered( jsonData.filter(entry => entry.Title.includes(e.target.value)/* .include(e.target.value) */) )
+  }
+
   return (
-    <div>
-      <h1> Hello Timo-chan </h1>
+    <div id='app'>
+      <input type="search" id='searchbar' onChange={handleChange}/>
+      <CardList cards={filtered}/>
     </div>
   )
 }
