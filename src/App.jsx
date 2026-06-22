e96bb239faa0911e02f0f333ad83613518111c67
AnaelleR <naths.raf@gmail.com>
2026-06-22 11:57:45 +0200

    feat/ start input search

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-
 import FilmDisplay from "./components/FilmDisplay"
 
 function App() {
@@ -7,6 +6,7 @@ function App() {
 
   return (
     <>
+
     <FilmDisplay />
     </>
   )
