4ff5ae2dc7d3fe7ffb3390147c064e4d13d63b4b
Typhaine Le Gales <typhaineLeGales@protonmail.com>
2026-06-22 14:14:03 +0200

    feat: rendered app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+// function App() {
+//   return (
+//     <div>
+//       <h1> Hello 🌞</h1>
+      
+//     </div>
+//   )
+// }
+
+// export default App
+import FilmList from "./components/FilmList"
+import Header from './components/Header'
+import { useState } from "react"
+
 function App() {
+  const [theme, setTheme] = useState("light")
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light")
+  }
+  
   return (
-    <div>
-      <h1> Hello 🌞 </h1>
+    <div className={`app ${theme}`}>
+      <Header
+        theme={theme}
+        toggleTheme={toggleTheme}
+      />
+      <FilmList theme={theme}/>
     </div>
   )
 }
