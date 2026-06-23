713d03d2a5d18915293f7107be56d799b91a9784
lougo27 <louanegouin@gmail.com>
2026-06-23 13:12:04 +0200

    fix: galere categorie

--- src/compoments/listFilm.jsx
+++ src/compoments/listFilm.jsx
@@ -4,20 +4,20 @@ import CardFilm from "./cardFilm";
 import films from "../assets/filmData.json";
 import "./listFilm.css";
 
+
+const allGenres = ["All", ...new Set(films.flatMap((film) => film.Genre.split(", ")))]; // J'ai chercher la fameuse méthode de tableau unique set
+
 function ListFilm() {
     const [search, setSearch] = useState(""); 
+    const [selectedGenre, setSelectedGenre] = useState("All"); // genre sélectionné
 
-    const filteredFilms = films.filter((film) =>
-        film.Title.toLowerCase().includes(search.toLowerCase())
-    );
+    const filteredFilms = films.filter((film) => {
+        const matchSearch = film.Title.toLowerCase().includes(search.toLowerCase());
+        const matchGenre = selectedGenre === "All" || film.Genre.includes(selectedGenre);
+        return matchSearch && matchGenre; // les deux filtres en même temps
+    });
 
     return (
-    // <div className="film-grid">
-    //     {films.map((film, index) => (
-    //     <CardFilm key={index} film={film} />
-    //     ))}
-    // </div>
-
     <div>
         <input // imput classique pour la barre de recherche
             type="text"
@@ -26,6 +26,19 @@ function ListFilm() {
             onChange={(e) => setSearch(e.target.value)}  // update a chaque lettre tapée
             className="recherche"
         />
+
+        {/* Boutons de genre */}
+        <div className="genre-filters">
+            {allGenres.map((genre) => (
+                <button
+                    key={genre}
+                    className={selectedGenre === genre ? "active" : ""}
+                    onClick={() => setSelectedGenre(genre)}
+                >
+                    {genre}
+                </button>
+            ))}
+        </div>
 
         {filteredFilms.length === 0 ? (
             <p>No movies found</p>  // phrase si y'a pas de résultat donc si y'a 0 dans le filtre
