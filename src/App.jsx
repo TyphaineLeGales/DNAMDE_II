e5ca75930824b0e37980abacf1c1d9195aca2b28
amandine <amandineserna450@gmail.com>
2026-06-23 11:38:29 +0200

    feat : input search

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import movies from "./assets/filmData.json";
 
 function App() {
   return (
-    <div className="movies-container">
-      {movies.map((movie, index) => (
-        <MovieCard
-          key={index}
-          movie={movie}
-        />
-      ))}
+    <div>
+      <div className="search-container">
+        <label>Search a movie </label>
+        <input type="text" />
+      </div>
+
+      <div className="movies-container">
+        {movies.map((movie, index) => (
+          <MovieCard
+            key={index}
+            movie={movie}
+          />
+        ))}
+      </div>
     </div>
   );
 }
