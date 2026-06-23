cfa2d4020c47d7e6c3b286f222b791e142f868bf
saraaaaaDM <desruelsarapro@gmail.com>
2026-06-23 15:04:40 +0200

    toogleTheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import FilmList from "./components/FilmList";
+import "./index.css";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div>
-      <h1>SaraFlix</h1>
+    <div className={`app ${theme}`}>
+      <h1>Bonjour Sara</h1>
+
+      <button onClick={toggleTheme} className="theme-btn">
+        {theme === "light" ? "Dark mode" : "Light mode"}
+      </button>
+
       <FilmList />
     </div>
   );
