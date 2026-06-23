763d8b5ff1d24c53f70b5b2109e878c5d6a20bdb
amandine <amandineserna450@gmail.com>
2026-06-23 12:12:25 +0200

    feat: search active

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
+import { useState } from "react";
 import MovieCard from "./movies";
 import "./search.css";
 import movies from "./assets/filmData.json";
 
 function App() {
+  
+  const [search, setSearch] = useState("");
+
+   const filteredMovies = movies.filter((movie) =>
+   movie.Title.toLowerCase().includes(search.toLowerCase())
+  );
   return (
     <div>
       <div className="search-container">
         <label>Search a movie </label>
-        <input type="text" />
+        <input
+        type="text"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+       />
       </div>
 
       <div className="movies-container">
-        {movies.map((movie, index) => (
-          <MovieCard
-            key={index}
-            movie={movie}
-          />
-        ))}
-      </div>
+  {filteredMovies.map((movie, index) => (
+    <MovieCard
+      key={index}
+      movie={movie}
+    />
+  ))}
+</div>
     </div>
   );
 }
