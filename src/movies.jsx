165af089d543aa31c63da8fceba3965c6a97fb2c
amandine <amandineserna450@gmail.com>
2026-06-23 11:19:19 +0200

    feat: images et notes + fix : css

--- src/movies.jsx
+++ src/movies.jsx
@@ -3,12 +3,18 @@ import "./movies.css";
 function MovieCard({ movie }) {
   return (
     <div className="movie-card">
+      <img
+        src={movie.Images[0]}
+        alt={movie.Title}
+        className="movie-image"
+      />
 
       <div className="movie-content">
         <h2>{movie.Title}</h2>
 
         <div className="movie-info">
           <span>{movie.Year}</span>
+          <span>⭐ {movie.imdbRating}</span>
         </div>
 
         <p className="genre">{movie.Genre}</p>
