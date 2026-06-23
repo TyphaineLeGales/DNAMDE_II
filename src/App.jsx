da97a04f36e3fb038aefd313bd75eb3a71898a46
saraaaaaDM <desruelsarapro@gmail.com>
2026-06-23 13:10:43 +0200

    Filtres + barre de recherche

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import FilmList from "./components/FilmList";
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Shadows+Into+Light+Two&display=swap');
-</style>
+
 function App() {
   return (
     <div>
       <h1>Bonjour Sara</h1>
-
       <FilmList />
     </div>
   );
