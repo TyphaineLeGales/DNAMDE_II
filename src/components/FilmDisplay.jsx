0e593251c847abf5c921c0fbe49ce35f4ba70fc4
AnaelleR <naths.raf@gmail.com>
2026-06-23 15:01:43 +0200

    fix/ api call

--- src/components/FilmDisplay.jsx
+++ src/components/FilmDisplay.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import FilmCard from "./FilmCard";
 import InputBar from "./InputBar";
 import TagList from "./TagList";
@@ -5,29 +6,48 @@ import { useEffect, useState } from "react";
 import data from "../assets/filmData.json";
 
 function FilmDisplay() {
+  const [dataLoad, isDataLoad] = useState(false);
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    const url = "https://ghibliapi.vercel.app/films/";
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        setFilms(json);
+        isDataLoad(true);
+      });
+  }, []);
+
   const [value, setValue] = useState(" ");
 
   const handleChange = (e) => {
     e.preventDefault();
     setValue(e.target.value);
+    console.log(films);
   };
 
-const [selectedTags, setSelectedTags] = useState([]);
+  const [selectedTags, setSelectedTags] = useState([]);
 
-const handleTagSelect = (tag, isActive) => {
-  setSelectedTags((prev) =>
-    isActive ? [...prev, tag] : prev.filter((t) => t !== tag),
-  );
-};
+  const handleTagSelect = (tag, isActive) => {
+    setSelectedTags((prev) =>
+      isActive ? [...prev, tag] : prev.filter((t) => t !== tag),
+    );
+  };
 
-const filteredFilms = data.filter((film) => {
-  const matchesSearch = film.Title.toLowerCase().includes(value.toLowerCase());
-  const matchesTags =
-    selectedTags.length === 0 ||
-    selectedTags.every((tag) => film.Genre.includes(tag));
-  return matchesSearch && matchesTags;
-});
+  const ghibliFilms = films.filter((film) => {
+    console.log(film);
+  });
 
+  const filteredFilms = data.filter((film) => {
+    const matchesSearch = film.Title.toLowerCase().includes(
+      value.toLowerCase(),
+    );
+    const matchesTags =
+      selectedTags.length === 0 ||
+      selectedTags.every((tag) => film.Genre.includes(tag));
+    return matchesSearch && matchesTags;
+  });
 
   return (
     <>
