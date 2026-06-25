14ff334d664cff11063068ee49e15a9420b27f9c
lucileguillo <lucile.guillo27@gmail.com>
2026-06-25 17:07:34 +0200

    feat: added variables

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import filmData from './assets/filmData.json?raw';
 import FilmList from './components/filmList';
 import GenreList from './components/genreList'
+import { ThemeProvider } from './components/theme';
 
 
 const jsonData = JSON.parse(filmData);
@@ -10,10 +11,10 @@ function App() {
 
   //recherche
   const [userInput, setUserInput] = useState("")
-  // const filteredFilms = jsonData.includes(userInput, setUserInput)
   const [selectedGenre, setSelectedGenre] = useState("All")
-  console.log(selectedGenre)
-    const isInSearch = (movie) => {
+  const [theme, setTheme] = useState('light');
+
+  const isInSearch = (movie) => {
     return movie.Title.toLowerCase().includes(userInput.toLowerCase())
   }
 
@@ -29,31 +30,27 @@ function App() {
     (movie) => isInSearch(movie) && isInGenreFilter(movie)
   ) 
 
-
-
- 
   const genres = jsonData.map(film => film.Genre.split(", "));
   const genresSet = [...new Set(['All', ...genres.flat()])];
 
-  // useEffect(() => {
-  //   filteredFilms
-  // }, [userInput])
 
   return (
-    <div className='films-container'>
-      <div className='search-container'>
-        <label>
-          Rechercher un film :
-          <input className='search-input' value={userInput} onChange={e => setUserInput(e.target.value)}/>
-        </label>
+    <ThemeProvider value={theme}>
+      <div className='films-container'>
+        <div className='search-container'>
+          <label>
+            Rechercher un film :
+            <input className='search-input' value={userInput} onChange={e => setUserInput(e.target.value)}/>
+          </label>
+        </div>
+        <GenreList genre={genresSet} setSelectedGenre={setSelectedGenre} selectedGenre={selectedGenre}/>
+        <FilmList film={filteredFilms}/>
+        
+        <button className={theme === "theme-btn"}>
+            <p>Dark mode</p>
+        </button>
       </div>
-      <GenreList genre={genresSet} setSelectedGenre={setSelectedGenre} selectedGenre={selectedGenre}/>
-      <FilmList film={filteredFilms}/>
-      
-      <button className="theme-btn">
-          <p>Dark mode</p>
-      </button>
-    </div>
+    </ThemeProvider>
   )
 
 }
