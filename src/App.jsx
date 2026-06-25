f4556ce446bc8f9ae256aa67b6ade11fab86c737
paulinovart <pauline.dierick@gmail.com>
2026-06-25 12:38:43 +0200

    feat : working filters youpiiiiiiiiii

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,51 @@
 import { useState } from "react";
+import { useEffect } from "react";
 //import Card from "./Card"
 import CardList from "./CardList.jsx";
 import Filters from "./Filters.jsx";
 import "./app.css";
 import jsonFilm from "./assets/filmData.json?raw";
 
+
 function App() {
   const filmsData = JSON.parse(jsonFilm);
   const [userInput, setUserInput] = useState("");
+  const [activeGenre, setActiveGenre] = useState("All");
+
+
+
 
   function handleChange(e) {
     setUserInput(e.target.value);
+
   }
 
-  const filteredFilms = filmsData.filter((film) =>
-    film.Title.toLowerCase().includes(userInput.toLowerCase()) ||
-    String(film.Year).includes(userInput) 
+
+
+  
+  const isTitleIsSearch = (film) => film.Title.toLowerCase().includes(userInput.toLowerCase())
+  const isGenreSelected = (film) => {
+    if(activeGenre===("All")){
+      return true
+
+    }else{
+      return film.Genre.includes(activeGenre) 
+    }
+   
+  
+  }
+  
+  const filteredFilms = filmsData.filter((film) => isTitleIsSearch(film) && isGenreSelected(film)
   );
 
-//const selecteGenre = 
+
 
   return (
     <div>
-      <Filters inputChange={handleChange} />
+      <Filters inputChange={handleChange} activeGenre={activeGenre} setActiveGenre={setActiveGenre}/>
       <CardList films={filteredFilms} />
     </div>
-    /* <div className="movies-container">
-
-
-      {filmsData.map(filmData => ( 
-        <Card data={filmData}/>
-
-
-      ))}
-
-      
-    </div>*/
-  );
+    );
 }
 
 export default App;
--- src/GenreChips.jsx
+++ src/GenreChips.jsx
@@ -1,18 +1,22 @@
-import{ useState } from 'react';
+
 import './filters.css';
 
 export default function GenreChips(props) {
-    const [isActive, setIsActive] = useState(false);
-    function handleSelected(selectedGenre){
+    // const [isActive, setIsActive] = useState(false);
+    
+    // function handleSelected(selectedGenre){
 
-        setIsActive(!isActive);
-        console.log(selectedGenre)
+    //     setIsActive(!isActive);
+    //     console.log(selectedGenre) }
 
+    function handleClick(){
+        props.selection(props.genre)
+    }
 
         
 
-    }
+    
     return (
-    <button  className={isActive ? "active" : ""}  onClick={() => handleSelected(props.genre) }>{props.genre}</button>
+    <button  className={props.selected ? "active" : ""}  onClick={handleClick}>{props.genre}</button>
       );
 }
