05d334476aabc626fc1360d2a224739d0df8ddc5
paulinovart <pauline.dierick@gmail.com>
2026-06-26 12:24:11 +0200

    feat : favorite state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import Search from  "./Search"
 import Favorites from "./Favorites"
+import { useState } from "react";
 
 function App() {
+
+
+    const [favorite, setFavorite] = useState(false);
+
+    const addToFavorites = () => {
+        setFavorite((isFavorite) => !isFavorite); //comme un toggle, change le favorite state
+        
+      };
   
     return <div>
-         <div> <Search/></div>
-         <div> <Favorites/></div>
+         <div> <Search addToFavorites={addToFavorites}/></div>
+         <div> <Favorites favorite={favorite}/></div>
     </div>
     
     }
--- src/stores/favorites.js
+++ src/stores/favorites.js
@@ -1,7 +1,7 @@
-import { create } from 'zustand'
+//import { create } from 'zustand'
 
-const useFavorites = create((set) ({
-favorites: [],
-addFavorites: (film) => set((state) => ({favorites: state.favorites.push(film) })),
-removeFavorite: (film) => set((state) => ({favorites: state. favorites.filter(f => f.title !== film.title) })),
-}))
+// const useFavorites = create((set) ({
+// favorites: [],
+// addFavorites: (film) => set((state) => ({favorites: state.favorites.push(film) })),
+// removeFavorite: (film) => set((state) => ({favorites: state. favorites.filter(f => f.title !== film.title) })),
+// }))
