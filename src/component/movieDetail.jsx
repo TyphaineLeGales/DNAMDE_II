fd2c83f10798a3fdd216e3ff319723a5ffcec333
Hatterrine <perrine.goget@yahoo.com>
2026-06-26 17:20:41 +0200

    add
    gérer si le film est trouvable ou pas

--- src/component/movieDetail.jsx
+++ src/component/movieDetail.jsx
@@ -6,6 +6,10 @@ function MovieDetail ({filmData}) {
     const { title } = useParams()
     const film = filmData.find((unFilm) => unFilm.title === title)
 
+    if (!film) {
+    return <p>404 Not Found</p>
+}
+
 }
 
 export default MovieDetail
