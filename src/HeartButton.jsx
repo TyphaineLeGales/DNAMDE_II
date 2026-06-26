a209e555db90b6c8c331a7a570483a3006d7d711
paulinovart <pauline.dierick@gmail.com>
2026-06-26 13:50:55 +0200

    refactor : heart button in separated component

---
+++ src/HeartButton.jsx
@@ -0,0 +1,17 @@
+
+import "./card.css";
+
+
+function HeartButton(){
+
+    return (
+        <div>
+            <img className="favorite-icon" src="src/assets/no-filled-heart-icon.svg" alt="" />
+        </div>
+    )
+
+}
+
+
+export default HeartButton;
+
