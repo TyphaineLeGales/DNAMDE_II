82bedd8d99bb963a4bf90e40c75988bf9048b089
paulinovart <pauline.dierick@gmail.com>
2026-06-26 14:19:35 +0200

    feat: implement FavoritesContext for managing favorite state

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import Search from  "./Search"
 import Favorites from "./Favorites"
-import { useState } from "react";
+import { useState, createContext } from "react";
 
 function App() {
-
-
     const [favorite, setFavorite] = useState(false);
+    
+    const FavoritesContext = createContext(favorite);
 
     const addToFavorites = () => {
         setFavorite((isFavorite) => !isFavorite); //comme un toggle, change le favorite state
+        console.log("favorite : ", favorite)
         
       };
   
-    return <div>
+    return (
+    <FavoritesContext value={favorite}>
          <div> <Search addToFavorites={addToFavorites}/></div>
          <div> <Favorites favorite={favorite}/></div>
-    </div>
+    </FavoritesContext>
+    )
+
     
     }
 
--- src/HeartButton.jsx
+++ src/HeartButton.jsx
@@ -1,14 +1,17 @@
 
+import { useContext } from "react";
 import "./card.css";
 
 
 function HeartButton(){
 
+    const favorite = useContext(FavoriteContext)
     return (
         <div>
-            <img className="favorite-icon" src="src/assets/no-filled-heart-icon.svg" alt="" />
+            { <img onClick={favorite} className="favorite-icon" src="src/assets/no-filled-heart-icon.svg" alt="" /> }
         </div>
     )
+
 
 }
 
