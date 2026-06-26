98ff8da1a3a10e4437bb67f3d1d0faecba425e91
Anaëlle RAFELANA <anaelle.rafelana@g-102a-01.intra.gobelins.fr>
2026-06-26 16:02:41 +0200

    feat/ use ref and focus

--- src/components/InputBar.jsx
+++ src/components/InputBar.jsx
@@ -1,9 +1,18 @@
 import ThemeSelect from "./ThemeSelect";
+import { useEffect, useRef } from "react";
 
 function InputBar(props) {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+    console.log(inputRef.current);
+  }, []);
+
   return (
     <div className="nav">
       <input
+        ref={inputRef}
         onChange={props.valueChange}
         name="input-bar"
         className="input-bar"
